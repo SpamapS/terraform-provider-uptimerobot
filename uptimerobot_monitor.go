acd3bb2530d7a0441538f10731404dd1d5dcd5f9
Clint Byrum <clint@fewbar.com>
2018-07-08 11:27:01 -0700

    fixes

--- uptimerobot_monitor.go
+++ uptimerobot_monitor.go
@@ -1,42 +1,42 @@
 package main
 
 import (
-        "github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/schema"
 )
 
-func uptimerobotMonitorCreate(d *schema.ResourceData,  interface{}) error {
-		return nil
+func uptimerobotMonitorCreate(d *schema.ResourceData, m interface{}) error {
+	return nil
 }
-func uptimerobotMonitorRead(d *schema.ResourceData,  interface{}) error {
-		return nil
+func uptimerobotMonitorRead(d *schema.ResourceData, m interface{}) error {
+	return nil
 }
-func uptimerobotMonitorUpdate(d *schema.ResourceData,  interface{}) error {
-		return nil
+func uptimerobotMonitorUpdate(d *schema.ResourceData, m interface{}) error {
+	return nil
 }
-func uptimerobotMonitorDelete(d *schema.ResourceData,  interface{}) error {
-		return nil
+func uptimerobotMonitorDelete(d *schema.ResourceData, m interface{}) error {
+	return nil
 }
 
 func uptimerobotMonitor() *schema.Resource {
-		return &schema.Resource{
-				Create: uptimerobotMonitorCreate,
-				Read: uptimerobotMonitorRead,
-				Update: uptimerobotMonitorUpdate,
-				Delete: uptimerobotMonitorDelete,
+	return &schema.Resource{
+		Create: uptimerobotMonitorCreate,
+		Read:   uptimerobotMonitorRead,
+		Update: uptimerobotMonitorUpdate,
+		Delete: uptimerobotMonitorDelete,
 
-				Schema: map[string]*schema.Schema{
-						"friendly_name": &schema.Schema{
-								Type:	schema.TypeString,
-								Required: true,
-						},
-						"url": &schema.Schema{
-								Type:	schema.TypeString,
-								Required: true,
-						},
-						"type": &schema.Schema{
-								Type:	schema.TypeString,
-								Required: true,
-						},
-				},
-		}
+		Schema: map[string]*schema.Schema{
+			"friendly_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"url": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"type": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
 }
