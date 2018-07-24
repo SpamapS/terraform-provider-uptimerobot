fbe0aaffd3eefcf0bfe2ab84f6be6397eda0ce20
Clint Byrum <clint@fewbar.com>
2018-07-24 02:26:07 +0200

    attempting refactor to use configurefunc

--- provider.go
+++ provider.go
@@ -2,12 +2,33 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/helper/schema"
+	"net/url"
+	"spamaps.org/uptimerobot"
 )
 
 func Provider() *schema.Provider {
-	return &schema.Provider{
+	var p *schema.Provider
+	p = &schema.Provider{
 		ResourcesMap: map[string]*schema.Resource{
 			"uptimerobot_monitor": uptimerobotMonitor(),
 		},
+	}
+	p.ConfigureFunc = providerConfigure(p)
+	return p
+}
+
+func providerConfigure(p *schema.Provider) schema.ConfigureFunc {
+	return func(d *schema.ResourceData) (interface{}, error) {
+		u, err := url.Parse("https://api.uptimerobot.com/v2")
+		if err != nil {
+			return nil, err
+		}
+		c := uptimerobot.Client{
+			BaseURL:    u,
+			UserAgent:  "terraform-provider-uptimerobot",
+			HttpClient: nil,
+			Api_key:    d.Get("api_key").(string),
+		}
+		return c, nil
 	}
 }
--- uptimerobot_monitor.go
+++ uptimerobot_monitor.go
@@ -3,36 +3,17 @@ package main
 import (
 	"fmt"
 	"github.com/hashicorp/terraform/helper/schema"
-	"net/url"
 	"spamaps.org/uptimerobot"
 	"strconv"
 )
 
-func makeClient() (*uptimerobot.Client, error) {
-	u, err := url.Parse("https://api.uptimerobot.com/v2")
-	if err != nil {
-		return nil, err
-	}
-	c := uptimerobot.Client{
-		BaseURL:    u,
-		UserAgent:  "terraform-provider-uptimerobot",
-		HttpClient: nil,
-		Api_key:    "aaaa",
-	}
-	return &c, nil
-}
-
 func uptimerobotMonitorCreate(d *schema.ResourceData, m interface{}) error {
-	c, err := makeClient()
-	if err != nil {
-		return err
-	}
 	mon := uptimerobot.Monitor{
 		Friendly_name: fmt.Sprintf("%s", d.Get("friendly_name")),
 		Url:           fmt.Sprintf("%s", d.Get("url")),
 		Monitor_type:  d.Get("type").(int),
 	}
-	err = c.CreateMonitor(&mon)
+	err := m.(*uptimerobot.Client).CreateMonitor(&mon)
 	if err != nil {
 		return err
 	}
@@ -48,15 +29,11 @@ func uptimerobotMonitorUpdate(d *schema.ResourceData, m interface{}) error {
 	return nil
 }
 func uptimerobotMonitorDelete(d *schema.ResourceData, m interface{}) error {
-	c, err := makeClient()
-	if err != nil {
-		return err
-	}
 	i, err := strconv.Atoi(d.Id())
 	if err != nil {
 		return err
 	}
-	return c.DeleteMonitor(i)
+	return m.(*uptimerobot.Client).DeleteMonitor(i)
 }
 
 func uptimerobotMonitor() *schema.Resource {
